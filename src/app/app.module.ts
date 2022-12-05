375ae3638800ea724ca6a5112d12b6699babc3d2
Carlos Azcarraga <tasajos@gmail.com>
2022-12-05 08:22:33 -0400

    adicion components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarUsuarioComponent } from './Components/agregar-usuario/agregar-usuario.component';
+import { ListarUsuarioComponent } from './Components/listar-usuario/listar-usuario.component';
+import { VerUsuarioComponent } from './Components/ver-usuario/ver-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarUsuarioComponent,
+    ListarUsuarioComponent,
+    VerUsuarioComponent
   ],
   imports: [
     BrowserModule,
